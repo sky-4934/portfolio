f0a710e15dbe4bd2a4054b96fa509eaccb88ed58
sky-4934 <4934akash@gmail.com>
2020-05-08 16:28:15 +0530

    sixth

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+def allblogs(request):
+    blog = Blog.objects
+    return render(request, 'blog/allblogs.html',{'blog':blog})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)  #pk means primary key
+    return render(request, 'blog/detail.html',{'detailblog':detailblog})
